233086acee5c59bc7a05b3deca0320b741d4b25a
Tarwat <t.uddin9121@gmail.com>
2022-06-18 15:06:48 +0100

    Update App component
    Added components to build app ui

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,19 @@
 import React from "react";
 
+import Wrapper from "./components/UI/Wrapper";
+import TodoAppBar from "./components/TodoAppBar/TodoAppBar";
+import Todos from "./components/Todos/Todos";
+import Todo from "./models/todo";
+
 function App() {
-  return <div></div>;
+  const newTodoHandler = (newTodo: Todo) => {};
+
+  return (
+    <Wrapper>
+      <TodoAppBar onNewTodo={newTodoHandler} />
+      <Todos todos={[]} />
+    </Wrapper>
+  );
 }
 
 export default App;
